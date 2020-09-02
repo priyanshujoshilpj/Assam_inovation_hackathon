55a31f590fa9d1ec58ddae6007e7153bbc0abd6e
Anurag Porel <anurag.porel19@st.niituniversity.in>
2020-09-03 03:49:26 +0530

    Basic Routing Done

--- Website/app.py
+++ Website/app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request
 
 app=Flask(__name__)
 
 @app.route("/")
+@app.route("/login")
 
 def home():
-    return render_template("index.html")
+    return render_template("login.html")
 
 app.run(debug=True)
