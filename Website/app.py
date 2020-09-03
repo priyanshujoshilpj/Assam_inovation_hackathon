f940fc4c0dc070810d144094fb328adc64425d18
Anurag Porel <anurag.porel19@st.niituniversity.in>
2020-09-03 14:29:21 +0530

    User authentication with temp local database

--- Website/app.py
+++ Website/app.py
@@ -2,10 +2,26 @@ from flask import Flask, render_template, url_for, request
 
 app=Flask(__name__)
 
+db={'Anurag':"anu256",'Priyanshu':"lpj123"}
+
 @app.route("/")
-@app.route("/login")
 
 def home():
     return render_template("login.html")
+
+@app.route("/login",methods=['POST','GET'])
+def login():
+    user=request.args['username']
+    pw=request.args['pass']
+
+    if user not in db:
+        info="Invalid User"
+        return render_template("home.html",info=info)
+    elif db[user]!=pw:
+        info="Wrong Password"
+        return render_template("home.html",info=info)
+    else:
+        info="Welcome "+user
+        return render_template("home.html",info=info)
 
 app.run(debug=True)
